fb1a3e78b4eb19e5f917b5f22fd3a77501b829e9
sufian <81829221+sufianfm@users.noreply.github.com>
2021-05-03 13:29:36 -0400

    Added animals.py

---
+++ chapter_4/pizzas.py
@@ -0,0 +1,7 @@
+pizzas = ['pepperoni', 'cheese', 'veggie', 'hawaiian', 'deep-dish']
+
+for pizza in pizzas:
+    print(pizza)
+    print(f"I love eating {pizza} pizza!\n")
+
+    print("If you couldn't tell, I really love pizza!")
