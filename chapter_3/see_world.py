ca5c98bd9272f8bd60890304bd414d274adb2474
sufian <81829221+sufianfm@users.noreply.github.com>
2021-05-03 12:39:03 -0400

    Added dinner_guests.py

--- chapter_3/see_world.py
+++ chapter_3/see_world.py
@@ -3,5 +3,35 @@ destinations = [
     'vienna',
     'delhi',
     'johannesburg',
-    'reykjavik',
-]
+    'reykjavik']
+
+print(destinations)
+
+# Using sorted() to print list in alphabetical order. Note this does not alter the list.
+print(sorted(destinations))
+
+# Proof that list order is unchanged
+print(destinations)
+
+# Using reverse sorted
+print(sorted(destinations, reverse = True))
+
+# Proof that list is unchanged
+print(destinations)
+
+# Reversing the lists
+destinations.reverse()
+print(destinations)
+
+# Reversing the list to bring it back to original
+destinations.reverse()
+print(destinations)
+
+# Sorting the list alphabetically
+destinations.sort()
+print(destinations)
+
+# Sorting the list reverse alphabetically
+destinations.sort(reverse = True)
+print(destinations)
+
