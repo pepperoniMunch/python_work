42756de77e63d1107a3ffddc02844ab7eca420cb
sufian <81829221+sufianfm@users.noreply.github.com>
2021-05-03 13:36:26 -0400

    Added animals.py

---
+++ chapter_4/animals.py
@@ -0,0 +1,11 @@
+fast_animals = ['cheetah', 'jaguar', 'cat']
+
+for fast_animal in fast_animals:
+    print(fast_animal)
+
+print("\n")
+
+for fast_animal in fast_animals:
+    print(f"A {fast_animal} can run really fast!")
+
+print("\nAny of these animals could beat a bike in a race!")
