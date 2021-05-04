25e099b0ae37d6ecacf02b22feaa8cbb4bca43e2
sufian <81829221+sufianfm@users.noreply.github.com>
2021-05-04 15:32:58 -0400

    Added tuple.py

---
+++ chapter_4/buffet.py
@@ -0,0 +1,15 @@
+# Define list
+foods = ('fries', 'torpedo shrimp', 'general tao chicken', 'green tea ice cream', 'steak')
+
+# For loop to print list items
+for item in foods:
+    print(item.title())
+print("\n")
+# Prove that tuples are immutable
+# foods[0] = 'sweet potato fries'
+
+# Menu revision
+foods = ('spaghetti', 'shrimp celeries', 'general tao chicken', 'green tea ice cream', 'steak')
+for items in foods:
+    print(items.title())
+
