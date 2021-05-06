c9cc52455ef7c1446f9ee60e37ee841d85feb9ce
sufian <81829221+sufianfm@users.noreply.github.com>
2021-05-06 14:23:50 -0400

    Added favorite_numbers.py

---
+++ chapter_6/person.py
@@ -0,0 +1,11 @@
+lauren = {
+    'first_name': 'lauren',
+    'last_name': 'smart',
+    'age': 30,
+    'city': 'toronto'
+}
+
+print(lauren['first_name'].title())
+print(lauren['last_name'].title())
+print(lauren['age'])
+print(lauren['city'].title())
