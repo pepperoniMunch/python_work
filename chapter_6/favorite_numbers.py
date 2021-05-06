15da023f0d589f398c847f408517ab344739383c
sufian <81829221+sufianfm@users.noreply.github.com>
2021-05-06 14:46:51 -0400

    Added user.py

---
+++ chapter_6/favorite_numbers.py
@@ -0,0 +1,13 @@
+favorite_numbers = {
+    'daniel': 4,
+    'wes': 7,
+    'steph': 19,
+    'ken': 7,
+    'shelley': 72,
+}
+
+print(f"Daniel's favorite number is {favorite_numbers['daniel']}.")
+print(f"Wes' favorite number is {favorite_numbers['wes']}.")
+print(f"Steph's favorite number is {favorite_numbers['steph']}.")
+print(f"Ken's favorite number is {favorite_numbers['ken']}.")
+print(f"Shelley's favorite number is {favorite_numbers['shelley']}.")
